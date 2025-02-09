7c0cd2d7044aed080c1e07d38f83e53db9bd22f3
Maverick D. Aece <aece.d.maverick@gmail.com>
2025-02-10 00:50:47 +0545

    fix: add validations to battery dto

--- src/main/java/org/hamal/abhishek/batterymgmt/application/dto/BatteryDto.java
+++ src/main/java/org/hamal/abhishek/batterymgmt/application/dto/BatteryDto.java
@@ -1,13 +1,18 @@
 package org.hamal.abhishek.batterymgmt.application.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record BatteryDto(
+        @NotBlank(message = "Say my name!")
         String name,
 
+        @Pattern(regexp = "^\\d+$", message = "Testing 1, 2, 3... INVALID POSTCODE")
         @JsonProperty("postcode")
         String postCode,
 
+        @Pattern(regexp = "^[1-9]\\d*$", message = "I need some positivity")
         @JsonProperty("capacity")
         int wattCapacity
 ) {
